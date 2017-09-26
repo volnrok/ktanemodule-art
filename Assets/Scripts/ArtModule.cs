3549b1f115f7d5fcbc0f67a096718d94632ae4d8
Sean Gilhuly <gilhulysean@gmail.com>
2017-09-26 17:17:09 -0400

    Initial commit; can draw on texture and select different brushes

---
+++ Assets/Scripts/ArtModule.cs
@@ -0,0 +1,197 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ArtModule : MonoBehaviour {
+
+	// Access with [hue, value]
+	private static Color[,] Palette = {
+		// Black / Gray / White
+		{new Color(0.15f,0.15f,0.15f),new Color(0.5f,0.5f,0.5f),new Color(1.0f,1.0f,1.0f)},
+		// Dark red / Red / Pink
+		{new Color(0.5f,0.1f,0.1f),new Color(1.0f,0.1f,0.1f),new Color(1.0f,0.65f,0.65f)},
+		// Brown / Orange / Tan
+		{new Color(0.5f,0.28f,0.1f),new Color(1.0f,0.5f,0.1f),new Color(1.0f,0.8f,0.5f)},
+		// Yellow
+		{new Color(1.0f,0.5f,0.1f),new Color(1.0f,0.9f,0.1f),new Color(1.0f,0.95f,0.5f)},
+		// Green
+		{new Color(0.1f,0.1f,0.1f),new Color(0.5f,0.5f,0.5f),new Color(1.0f,1.0f,1.0f)},
+		// Blue
+		{new Color(0.1f,0.1f,0.1f),new Color(0.5f,0.5f,0.5f),new Color(1.0f,1.0f,1.0f)},
+		 // Purple
+		{new Color(0.1f,0.1f,0.1f),new Color(0.5f,0.5f,0.5f),new Color(1.0f,1.0f,1.0f)}
+	};
+
+	private static int[] BrushSizes = {
+		60,
+		24,
+		12
+	};
+
+	public Transform CanvasRoot;
+	public Transform PaintSurface;
+	public Collider CollisionSurface;
+	public KMSelectable[] Brushes;
+
+	private KMSelectable Selectable;
+	private KMBombInfo BombInfo;
+	private KMBombModule BombModule;
+	private KMAudio Audio;
+
+	private static ArtModule selectedModule;
+	private bool isActive = false;
+	private bool isComplete = false;
+	private bool isSelected = true;
+	private bool isSelectingModule = false; // Need to make sure the mouse is released after selecting before paint is enabled
+
+	private Texture2D canvas;
+	private Vector2 lastPosition;
+	private Vector2 nullPosition = new Vector2 (-1, -1);
+	private int brush = 0;
+	private int hueIndex = 0;
+	private int valueIndex = 1;
+
+	private int moduleId;
+	private static int moduleIdCounter = 1;
+
+	private int CanvasWidth = 256;
+	private int CanvasHeight = 384;
+	//private int CanvasWidth = 64;
+	//private int CanvasHeight = 96;
+	private Vector2 CanvasSize;
+
+	private Vector3 OutPosition = new Vector3 (0, 0, 0.09f);
+	private Vector3 InPosition = new Vector3 (0, 0, 0.07f);
+	private float MoveSpeed = 0.15f;
+
+	protected void Start () {
+		moduleId = moduleIdCounter++;
+		CanvasSize = new Vector2 (CanvasWidth, CanvasHeight);
+
+		Selectable = GetComponent<KMSelectable> ();
+		BombInfo = GetComponent<KMBombInfo> ();
+		BombModule = GetComponent<KMBombModule> ();
+		Audio = GetComponent<KMAudio> ();
+
+		canvas = new Texture2D (CanvasWidth, CanvasHeight, TextureFormat.ARGB32, false);
+		canvas.wrapMode = TextureWrapMode.Clamp;
+		for (int y = 0; y < CanvasHeight; y++) {
+			for (int x = 0; x < CanvasWidth; x++) {
+				canvas.SetPixel (x, y, Color.white);
+			}
+		}
+		canvas.Apply ();
+		PaintSurface.GetComponent<Renderer> ().material.mainTexture = canvas;
+
+		for (int i = 0; i < Brushes.Length; i++) {
+			int j = i;
+			Brushes [j].OnInteract += delegate() {
+				brush = j;
+				return false;
+			};
+		}
+
+		BombModule.OnActivate += OnActivate;
+		Selectable.OnInteract += OnModuleSelect;
+		Selectable.OnCancel += OnModuleDeselect;
+		foreach (KMSelectable s in Brushes) {
+			s.OnCancel += OnModuleDeselect;
+		}
+		//Selectable.OnCancel = new KMSelectable.OnCancelHandler (this.OnModuleDeselect);
+
+		UpdatePalette (0, 0);
+	}
+
+	protected void OnActivate() {
+		isActive = true;
+	}
+
+	protected bool OnModuleSelect() {
+		if (moduleId == 1) {
+			Audio.PlayGameSoundAtTransform (KMSoundOverride.SoundEffect.BombDefused, transform);
+		} else {
+			Audio.PlayGameSoundAtTransform (KMSoundOverride.SoundEffect.BombExplode, transform);
+		}
+		selectedModule = this;
+		isSelected = true;
+		isSelectingModule = true;
+		return true;
+	}
+
+	public bool OnModuleDeselect() {
+		Audio.PlayGameSoundAtTransform (KMSoundOverride.SoundEffect.GameOverFanfare, transform);
+		isSelected = false;
+		return true;
+	}
+
+	protected void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			isSelectingModule = false;
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			lastPosition = nullPosition;
+		}
+
+		if (isSelected && this == selectedModule && !isSelectingModule && Input.GetMouseButton (0)) {
+			// Make sure the canvas is facing towards the camera (z direction from canvas)
+			if (Vector3.Dot (Camera.main.transform.TransformVector (Vector3.forward), PaintSurface.TransformVector (Vector3.forward)) > 0) {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+				// Do a raycast to the canvas
+				if (CollisionSurface.Raycast (ray, out hit, 100)) {
+					// Calculate the position and do the paint operation
+					Vector2 hitPosition = CanvasRoot.InverseTransformPoint (hit.point);
+					if (lastPosition == nullPosition) {
+						lastPosition = hitPosition;
+					}
+					DoPaint (lastPosition, hitPosition);
+					lastPosition = hitPosition;
+				} else {
+					lastPosition = nullPosition;
+				}
+			}
+		}
+
+		for (int i = 0; i < Brushes.Length; i++) {
+			if (i == brush) {
+				Brushes [i].transform.localPosition = Vector3.Lerp (Brushes [i].transform.localPosition, OutPosition, MoveSpeed);
+			} else {
+				Brushes [i].transform.localPosition = Vector3.Lerp (Brushes [i].transform.localPosition, InPosition, MoveSpeed);
+			}
+		}
+	}
+
+	protected void DoPaint(Vector2 v0, Vector2 v1) {
+		TextureDraw.DrawLine (canvas, Vector2.Scale (v0, CanvasSize), Vector2.Scale (v1, CanvasSize), BrushSizes [brush], Palette [0, 0]);
+		canvas.Apply ();
+	}
+
+	protected void UpdatePalette(int hueMod, int valueMod) {
+		hueIndex = ClampHue (hueIndex + hueMod);
+		valueIndex = ClampValue (valueIndex + valueMod);
+	}
+
+	protected int ClampHue(int hue) {
+		if (hue < 0) {
+			return Palette.GetLength (0) - 1;
+		} else if (hue >= Palette.GetLength (0)) {
+			return 0;
+		}
+		return hue;
+	}
+
+	protected int ClampValue(int value) {
+		if (value < 0) {
+			return 0;
+		} else if (value >= Palette.GetLength (1)) {
+			return Palette.GetLength (1) - 1;
+		}
+		return value;
+	}
+
+	protected void ChangeZ(Transform t, float z) {
+		// Helper function for vertically positioning buttons
+		t.localPosition = new Vector3(t.localPosition.x, t.localPosition.y, z);
+	}
+}
---
+++ Assets/Scripts/TextureDraw.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TextureDraw {
+	private static int Max(int a, int b) {
+		return a > b ? a : b;
+	}
+
+	private static int Min(int a, int b) {
+		return a < b ? a : b;
+	}
+
+	private static Vector2 ComponentMax(Vector2[] vectors) {
+		Vector2 max = vectors [0];
+		for (int i = 0; i < vectors.Length; i++) {
+			max = Vector2.Max (max, vectors [i]);
+		}
+		return max;
+	}
+
+	private static Vector2 ComponentMin(Vector2[] vectors) {
+		Vector2 min = vectors [0];
+		for (int i = 0; i < vectors.Length; i++) {
+			min = Vector2.Min (min, vectors [i]);
+		}
+		return min;
+	}
+	
+	public static void DrawLine(Texture2D tex, Vector2 start, Vector2 end, float width, Color c) {
+		// Convert the line to a polygon
+		float angle = Mathf.Atan2 (end.x - start.x, end.y - start.y);
+		//Debug.Log ("Direction is: " + (end - start).ToString ());
+		Vector2 perpendicular = new Vector2 (Mathf.Sin (angle + Mathf.PI / 2), Mathf.Cos (angle + Mathf.PI / 2)) * width / 2;
+		//Debug.Log ("Perpendicular is: " + perpendicular.ToString ());
+		Vector2[] vertices = new Vector2[4] {
+			start - perpendicular,
+			start + perpendicular,
+			end + perpendicular,
+			end - perpendicular
+		};
+
+		RasterPolygon (tex, vertices, c);
+		RasterCircle (tex, (int)start.x, (int)start.y, (int)(width / 2), c);
+		RasterCircle (tex, (int)end.x, (int)end.y, (int)(width / 2), c);
+	}
+
+	// Polygon rasterization algorithm:
+	// http://alienryderflex.com/polygon_fill/
+	public static void RasterPolygon(Texture2D tex, Vector2[] poly, Color c) {
+		int nodes, i, j, temp;
+		int[] nodeX = new int[poly.Length];
+
+		// Centre the int pixels by shifting the polygon backwards half a pixel TODO: Does this help?
+		Vector2 offset = new Vector2(-0.5f, -0.5f);
+		for (i = 0; i < poly.Length; i++) {
+			poly [i] = poly [i];// + offset;
+		}
+
+		// Calculate boundaries
+		Vector2 max = ComponentMax (poly);
+		Vector2 min = ComponentMin (poly);
+		int maxX = Min ((int)(max.x + 1), tex.width);
+		int maxY = Min ((int)(max.y + 1), tex.height);
+		int minX = Max ((int)(min.x), 0);
+		int minY = Max ((int)(min.y), 0);
+
+		// Loop through rows of the image
+		for (int y = minY; y < maxY; y++) {
+			// Build a list of nodes
+			nodes = 0;
+			j = poly.Length - 1;
+			for (i = 0; i < poly.Length; i++) {
+				if (poly [i].y < y && poly [j].y >= y || poly [j].y < y && poly [i].y >= y) {
+					nodeX [nodes++] = (int)(poly [i].x + (y - poly [i].y) / (poly [j].y - poly [i].y) * (poly [j].x - poly [i].x));
+				}
+				j = i;
+			}
+
+			// Sort the nodes
+			i = 0;
+			for (i = 0; i < nodes; i++) {
+				for (j = i + 1; j < nodes; j++) {
+					if (nodeX[i] > nodeX[j]) {
+						temp = nodeX[i];
+						nodeX[i] = nodeX[j];
+						nodeX[j] = temp;
+					}
+				}
+			}
+
+			// Fill the pixels between node pairs
+			for (i = 0; i < nodes; i += 2) {
+				if (nodeX [i] >= maxX) {
+					break;
+				} else if (nodeX [i + 1] > minX) {
+					if (nodeX [i] < minX) {
+						nodeX [i] = minX;
+					}
+					if (nodeX [i + 1] > maxX) {
+						nodeX [i + 1] = maxX;
+					}
+					for (int x = nodeX [i]; x < nodeX [i + 1]; x++) {
+						tex.SetPixel (x, y, c);
+					}
+				}
+			}
+		}
+	}
+
+	private static void HorizontalLine(Texture2D tex, int x0, int x1, int y, Color c) {
+		if (y < 0 || y >= tex.height) {
+			return;
+		}
+		x0 = Max (x0, 0);
+		x1 = Min (x1, tex.width - 1);
+		for (int x = x0; x <= x1; x++) {
+			tex.SetPixel (x, y, c);
+		}
+	}
+
+	// https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+	public static void RasterCircle(Texture2D tex, int x0, int y0, int radius, Color c) {
+		int x = radius-1;
+		int y = 0;
+		int dx = 1;
+		int dy = 1;
+		int err = dx - (radius << 1);
+
+		while (x >= y)
+		{
+			HorizontalLine (tex, x0 - x, x0 + x, y0 + y, c);
+			HorizontalLine (tex, x0 - x, x0 + x, y0 - y, c);
+			HorizontalLine (tex, x0 - y, x0 + y, y0 + x, c);
+			HorizontalLine (tex, x0 - y, x0 + y, y0 - x, c);
+
+			if (err <= 0)
+			{
+				y++;
+				err += dy;
+				dy += 2;
+			}
+			if (err > 0)
+			{
+				x--;
+				dx += 2;
+				err += (-radius << 1) + dx;
+			}
+		}
+	}
+}
